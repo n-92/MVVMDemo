5a5da71dea867edb816e3696b5c4d168cfc4b707
Aung Naing Oo <Aung Naing Oo>
2018-03-04 19:40:42 +0800

    First commit 'MVVM Demo'

---
+++ Chap1/ProductsManagement.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Chap1
+{
+    /// <summary>
+    /// Interaction logic for ProductsManagement.xaml
+    /// </summary>
+    public partial class ProductsManagement : Page
+    {
+        ProductsFactory factory; 
+        public ProductsManagement()
+        {
+            InitializeComponent();
+            factory = new ProductsFactory();
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            dataGrid.ItemsSource = factory.FindProducts(textBox.Text);
+        }
+    }
+}
---
+++ Chap1/ProductsManagmentMVVMViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chap1
+{
+    class ProductsManagmentMVVMViewModel:Notifier
+    {
+        private string searchInput;
+
+        public string SearchInput
+        {
+            get { return searchInput; }
+            set
+            {
+                searchInput = value;
+                base.OnPropertyChanged("SearchInput");
+                OnSearchInputChanged();
+            }
+        }
+
+        private IEnumerable<Product> foundProducts; 
+
+        public IEnumerable<Product> FoundProducts
+        {
+            get { return foundProducts; }
+            set
+            {
+                foundProducts = value;
+                OnPropertyChanged("FoundProducts");
+            }
+        }
+
+        private Product selectedProduct;
+        public Product SelectedProduct
+        {
+            get { return selectedProduct; }
+            set
+            {
+                selectedProduct = value;
+                OnPropertyChanged("SelectedProduct");
+            }
+        }
+
+
+        ProductsFactory factory = new ProductsFactory();
+        public ProductsManagmentMVVMViewModel()
+        {
+            FoundProducts = Enumerable.Empty<Product>();
+        }
+        
+
+        private void OnSearchInputChanged()
+        {
+            SelectedProduct = null;
+            FoundProducts = factory.FindProducts(searchInput);
+        }
+    }
+}
